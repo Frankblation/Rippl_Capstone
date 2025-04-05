43a8daa8717e7b9bee171a1a96324ac50cfe9d57
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-04-05 14:08:39 -0500

    removing login and replacing with create-profile for sav to test

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,27 +31,30 @@ function RootLayoutNav() {
         <Stack.Screen name="landing" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="signup" options={{ headerShown: false }} />
-        
+
         {/* Onboarding screens - auth required */}
-        <Stack.Screen 
-          name="customize-profile" 
-          options={{ 
+        <Stack.Screen
+          name="create-profile"
+          options={{
             headerShown: false,
             // Prevent going to these screens if not authenticated
-            headerBackVisible: false
-          }} 
+            headerBackVisible: false,
+          }}
         />
-        <Stack.Screen name="create-interests" options={{ headerShown: false, headerBackVisible: false }} />
+        <Stack.Screen
+          name="create-interests"
+          options={{ headerShown: false, headerBackVisible: false }}
+        />
         <Stack.Screen name="welcome" options={{ headerShown: false, headerBackVisible: false }} />
-        
+
         {/* Main app - auth required */}
-        <Stack.Screen 
-          name="(tabs)" 
-          options={{ 
+        <Stack.Screen
+          name="(tabs)"
+          options={{
             headerShown: false,
             // Prevent going back to auth screens
-            headerBackVisible: false
-          }} 
+            headerBackVisible: false,
+          }}
         />
         <Stack.Screen name="+not-found" />
       </Stack>
