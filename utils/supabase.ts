b58de6155ad4f378612002f5623b373e2be3f648
chdrchz <saavannaa.rose@gmail.com>
2025-04-04 19:23:39 -0500

    changing supabase.ts to base root of what supabase tells us to have it like + auth fopr debug purposes

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,39 +1,18 @@
 import { AppState } from 'react-native';
-import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
-
-if (!supabaseUrl || (!supabaseAnonKey && !supabaseServiceRoleKey)) {
-  throw new Error('Supabase URL or keys are not defined in environment variables.');
-}
-
-// Determine the runtime environment and set the appropriate key and storage
-let supabaseKey: string;
-let storage: any;
-
-// if (typeof window === 'undefined') {
-//   // Node.js environment
-//   supabaseKey = supabaseServiceRoleKey; // Use service role key in Node.js
-//   const { LocalStorage } = require('node-localstorage');
-//   storage = new LocalStorage('./scratch');
-// } else {
-//   // Mobile or browser environment
-//   supabaseKey = supabaseAnonKey; // Use anon key in mobile/browser
-//   const AsyncStorage = require('@react-native-async-storage/async-storage').default;
-//   storage = AsyncStorage;
-// }
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  })
 
 // Tells Supabase Auth to continuously refresh the session automatically
 // if the app is in the foreground. When this is added, you will continue
