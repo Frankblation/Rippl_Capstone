221387a94768a45b52825cb6669ba71c07178130
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-04-10 17:58:15 -0500

    Text Input props added to set keyboardtype to default and autocapitilize

--- components/BottomSheetTextInput.tsx
+++ components/BottomSheetTextInput.tsx
@@ -2,7 +2,9 @@ import React, { forwardRef } from 'react';
 import { TextInput, TextInputProps, StyleSheet } from 'react-native';
 
 const BottomSheetTextInput = forwardRef<TextInput, TextInputProps>((props, ref) => {
-  return <TextInput ref={ref} {...props} style={[styles.input, props.style]} />;
+  return (
+    <TextInput ref={ref} keyboardType="default" {...props} style={[styles.input, props.style]} />
+  );
 });
 
 const styles = StyleSheet.create({
